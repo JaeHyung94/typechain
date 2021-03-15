f3680f00130f1e17a857b6f9fa563ff918d58159
JaeHyung94 <jhpark.main@gmail.com>
2021-03-15 23:34:45 +0900

    Typechain: typescript tutorial

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const name = "Jay", age = 27, gender = "male"
+
+const sayHi = (name:string, age:number, gender:string): string => {
+    return (`Hello, ${name}, you are ${age} years old ${gender}`)
+}
+
+sayHi(name, age, gender)
+
+export {}  
